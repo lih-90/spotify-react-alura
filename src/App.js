3995ab140194247bf49ee86915c840b0aa5c9f8c
Aline Ometto <aline.ometto90@gmail.com>
2025-02-01 16:17:35 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Sidebar/>
+    <Footer/>
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,65 @@
+import './Sidebar.css';
+import logoSpotify from '../../assets/icons/logo-spotify.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSearch, faGlobe, faBook, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <nav className="sidebar__navigation">
+                <div className="logo">
+                    <a href="">
+                        <img src={logoSpotify} alt="Logo" />
+                    </a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="">
+                            <span><FontAwesomeIcon icon={faHome} /></span>
+                            <span>Início</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span><FontAwesomeIcon icon={faSearch} /></span>
+                            <span>Buscar</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="library">
+                <div className="library__content">
+                    <button className="library__button">
+                    <a href="">
+                    <span><FontAwesomeIcon icon={faBook} /></span></a>
+                        <span className="fa fas fa-book"></span>
+                        <span>Sua Biblioteca</span>
+                    </button>
+                    <span className="fa fa-plus"></span>
+                    <a href="">
+                    <span><FontAwesomeIcon icon={faPlus} /></span></a>
+                </div>
+                <section className="section-playlist">
+                    <div className="section-playlist__content">
+                        <span className="text title">Crie sua primeira playlist</span>
+                        <span className="text subtitle">É fácil, vamos te ajudar.</span>
+                        <button className="section-playlist__button">
+                            <span>Criar playlist</span>
+                        </button>
+                    </div>
+                </section>
+                <div className="cookies">
+                    <a href="">Cookies</a>
+                </div>
+                <div className="languages">
+                    <button className="languages__button">
+                        <span><FontAwesomeIcon icon={faGlobe} /></span>
+                        <span>Português do Brasil</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Sidebar;
